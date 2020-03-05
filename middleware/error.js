ce0027a88d63acd68b84f4ed4e2b06b8e7a79e62
chrismcglynn <chris@mcglynnenterprises.com>
2020-03-05 18:21:17 -0500

    create custom ErrorResponse class

--- middleware/error.js
+++ middleware/error.js
@@ -2,9 +2,9 @@ function errorHandler(error, req, res, next) {
   // Log to console for dev
   console.log(error.stack.red);
 
-  res.status(500).json({
+  res.status(error.statusCode || 500).json({
     success: false,
-    errror: error.message
+    errror: error.message || 'Server Error'
   });
 }
 
